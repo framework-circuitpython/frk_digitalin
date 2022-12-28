502a8866a951054229d1477cb56e59abde6c1f1e
Nathan Woody <nathan@nathanwoody.com>
2022-12-28 05:36:41 +0000

    Commit to latest

--- src/frk_digitalin.py
+++ src/frk_digitalin.py
@@ -1,44 +1,37 @@
-from framework import Driver
-import digitalio
+import asyncio
+from digitalio import DigitalInOut, Pull
 
-class DigitalIn(Driver):
-    _defaults = {'sleep': 0.01,
-                 'pull': 'UP',
-                 'invert': False,
-                 'value': False,
-                 'event': False,
-                 'rising': False,
-                 'falling': False,
-                 'on_event': [],
-                 'on_rising': [],
-                 'on_falling': []}
-
-    _get_set_del = {'sleep': 'g',
-                    'pull': 'g',
-                    'invert': 'gs',
-                    'value': 'g',
-                    'event': 'gs',
-                    'rising': 'gs',
-                    'falling': 'gs',
-                    'on_event': 's',
-                    'on_rising': 's',
-                    'on_falling': 's'}
-
-    _pull_modes = {'NONE': None,
-                   'UP': digitalio.Pull.UP,
-                   'DOWN': digitalio.Pull.DOWN}
-
+class DigitalIn:
+    poll_rate = 50.0
+    value = False
+    pull = "UP"
+    invert = False
+    enable = True
+    event = False
+    rising = False
+    falling = False
+    on_event = []
+    on_rising = []
+    on_falling = []
+    
+    _pulls = {"NONE": None,
+              "UP": Pull.UP,
+              "DOWN": Pull.DOWN}
+    
     def _init_device(self):
-        self._device = digitalio.DigitalInOut(self._pin)
-        self._device.switch_to_input(pull=self._pull_modes[self._pull])
-        self.__value = self._value = self._device.value ^ self._invert
-
-    def _loop(self):
-        self._value = self._device.value ^ self._invert
-        if self._value and not self.__value:
-            self._handle_event('rising', self._value)
-        if not self._value and self.__value:
-            self._handle_event('falling', self._value)
-        if self._value != self.__value:
-            self._handle_event('event', self._value)
-            self.__value = self._value
+        self._device = DigitalInOut(self._pin)
+        self._device.switch_to_input(pull=self._pulls[self._pull])
+        self._value = self._previous = self._device.value
+        self._sleep = 1.0 / self._poll_rate
+    
+    def _run(self):
+        while True:
+            self._value = self._device.value ^ self._invert & self._enable
+            if self._value != self._previous:
+                self._handle_event('event', self._value)
+                if self._value:
+                    self._handle_event('rising')
+                else:
+                    self._handle_event('falling')
+                self._previous = self._value
+            await asyncio.sleep(self._sleep)
